bf76152fda5bafdf7b0414309aa44ba284c2de9f
Dobuzi <kimjw7981@gmail.com>
2020-07-08 20:44:00 +0900

    cache and styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const queryMovies = gql`
     {
@@ -11,12 +13,70 @@ const queryMovies = gql`
     }
 `;
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`;
+
+const Header = styled.header`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+    font-size: 35px;
+`;
+
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
+`;
+
 export default () => {
     const { loading, data } = useQuery(queryMovies);
-    if (loading) {
-        return "Loading...";
-    }
-    if (data && data.movies) {
-        return data.movies.map((movie) => <h1 key={movie.id}>{movie.id}</h1>);
-    }
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo Movie App</Title>
+                <Subtitle>React, Apollo and GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies && (
+                <Movies>
+                    {data.movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            bg={movie.medium_cover_image}
+                        />
+                    ))}
+                </Movies>
+            )}
+        </Container>
+    );
 };
